bba3553eae7dbe9e3ef64611a1bce00bf167b266
hydralisk1 <hydralisk.ks@gmail.com>
2020-11-16 16:22:11 -0500

    project

--- whatwehave.py
+++ whatwehave.py
@@ -1,4 +1,22 @@
-from glob import glob
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 def what_we_have():
-    return [company.split("\\")[1].split("_")[0] for company in glob("static/cnn_models/*")]
+    uri = "postgres://postgres:hy046790hy@localhost:5432/project_3"
+    engine = create_engine(uri)
+    Base = automap_base()
+    Base.prepare(engine, reflect = True)
+
+    Predicted = Base.classes.predicted
+    
+    session = Session(engine)
+
+    query = session.query(Predicted.code)
+
+    data = []
+    # Run queries to load data from the database server
+    for row in query:
+        data.append(row.code)
+        
+    return data
